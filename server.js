84e437f38cd40a9f83372ba797535435c6c3bf85
martganan24 <mart.ganan24@gmail.com>
2025-03-05 20:45:20 +0800

    Updated project structure: moved files into public/, adjusted paths in server.js

--- server.js
+++ server.js
@@ -4,23 +4,22 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the assets folder
-app.use(express.static(path.join(__dirname, 'assets')));
-app.use(express.static(path.join(__dirname, 'css')));
-app.use(express.static(path.join(__dirname, 'js')));
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // Serve the index.html as the main page
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Serve the login and register pages
+// Serve login page
 app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "login.html"));
+    res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
+// Serve register page
 app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname, "register.html"));
+    res.sendFile(path.join(__dirname, "public", "register.html"));
 });
 
 // Start the server
